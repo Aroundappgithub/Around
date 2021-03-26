e7af9a2616088d2b274ef90c3001612c0b6e3922
Millad Nooristani <millad.nooristani@piscesworldwide.com>
2021-03-26 14:45:06 -0400

    Updated location and contacts permission handling. Implemented ability to read contacts list.

--- app/src/main/java/com/example/nearfriends/ContactsListActivity.java
+++ app/src/main/java/com/example/nearfriends/ContactsListActivity.java
@@ -1,19 +1,66 @@
 package com.example.nearfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.widget.Toast;
+
+import static android.Manifest.permission.READ_CONTACTS;
 
 public class ContactsListActivity extends AppCompatActivity {
+    private static final int CONTACT_REQUEST_CODE = 10002;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
+
+        if (ContextCompat.checkSelfPermission(this, READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            fetchContacts();
+        } else {
+            //Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{READ_CONTACTS}, CONTACT_REQUEST_CODE);
+            } else {
+                //No explanation needed, we can request the permission
+                ActivityCompat.requestPermissions(this, new String[]{READ_CONTACTS}, CONTACT_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CONTACT_REQUEST_CODE: {
+                //If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    //permission was granted, start location updates
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                    fetchContacts();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Permission denied, we need permissions to operate", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     private void fetchContacts() {
